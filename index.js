a10fdc12b7c0a266ce0378b598788bf99a9fdff4
abu-tahir-sk <abutahir2626@gmail.com>
2025-10-12 09:11:55 -0900

    gitignore

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://raise-up:raise-up@cluster0.qha6rup.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
+const uri =
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qha6rup.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+console.log(uri)
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -20,8 +23,8 @@ const client = new MongoClient(uri, {
 
 const run = async () => {
   try {
-//     await client.connect();
-//     await client.bd("raise-up").command({ ping: 1 });
+    //     await client.connect();
+    //     await client.bd("raise-up").command({ ping: 1 });
     console.log("pinged .............");
   } finally {
     // await client.close()
